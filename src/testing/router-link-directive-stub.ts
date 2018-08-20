89bd0fa88eb7e554cff285ef37e4c8389c0970cb
Bethany Warner <beth@wiredviews.com>
2018-08-20 14:34:42 -0400

    refactor(comments): trim unnecessary comments and remove unused file

---
+++ src/testing/router-link-directive-stub.ts
@@ -0,0 +1,36 @@
+import { Directive, Input } from '@angular/core';
+
+// export for convenience.
+export { RouterLink } from '@angular/router';
+
+/* tslint:disable:directive-class-suffix */
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[routerLink]',
+  // tslint:disable-next-line:use-host-property-decorator
+  host: { '(click)': 'onClick()' }
+})
+export class RouterLinkDirectiveStub {
+  // tslint:disable-next-line:no-input-rename
+  @Input('routerLink')
+  linkParams: any;
+  navigatedTo: any = null;
+
+  onClick() {
+    this.navigatedTo = this.linkParams;
+  }
+}
+
+/// Dummy module to satisfy Angular Language service. Never used.
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [RouterLinkDirectiveStub]
+})
+export class RouterStubsModule {}
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
