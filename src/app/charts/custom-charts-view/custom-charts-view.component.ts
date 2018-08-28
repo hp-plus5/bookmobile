a7ce25946854d60ad004894805897482f5a7b6e4
Bethany Warner <beth@wiredviews.com>
2018-08-28 12:36:23 -0400

    feat(custom-charts-view): create view component for display of user-desired data

---
+++ src/app/charts/custom-charts-view/custom-charts-view.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ChartType } from '@app/charts/_models/chart-request';
+import { ChartResponse } from '@app/charts/_models/chart-response';
+import { ChartService } from '@app/charts/chart.service';
+import { single } from '@app/charts/charts-response.fixture.spec';
+import { ModalService } from '@app/core/modal/modal.service';
+
+@Component({
+  selector: 'app-custom-charts-view',
+  templateUrl: './custom-charts-view.component.html',
+  styleUrls: ['./custom-charts-view.component.css'],
+})
+export class CustomChartsViewComponent implements OnInit {
+  @Input()
+  chartResponse = new ChartResponse();
+  // TODO: this is likely unnecessary
+  chosenChartType!: ChartType;
+  single: any[] = [];
+  multi: any[] = [];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Genre';
+  showYAxisLabel = true;
+  yAxisLabel = 'Number of Books';
+  showDataLabel = true;
+  colorScheme = 'cool';
+  constructor(
+    private modalService: ModalService,
+    private chartService: ChartService,
+  ) {
+    Object.assign(this, { single });
+  }
+  ngOnInit(): void {
+    if (!this.chartResponse) {
+      // this.getChartResponse(this.chartResponse);
+    }
+  }
+
+  getChartResponse(): void {
+    this.chartService
+      .getChartData()
+      .subscribe(newResponse => (this.chartResponse = newResponse));
+  }
+}
