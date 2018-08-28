838fe42b4e09345edd837791bea237c45ed54a2f
Bethany Warner <beth@wiredviews.com>
2018-08-28 16:40:14 -0400

    refactor(modal): correct typo within modal component class

--- src/app/core/modal/modal.component.ts
+++ src/app/core/modal/modal.component.ts
@@ -9,7 +9,7 @@ import { IModalOptions, ModalResult, ModalService } from '@app/core/modal/modal.
 })
 export class ModalComponent implements OnInit {
   modalOptions: IModalOptions | undefined;
-  // with "modalOptions!: ModalOptions" (no | undefined) lint is warning us that this variable may show up as undefined later in our code. but because we as the dev know that it's defined within the ngOnInit, we add the ! to tell lint to go away, we know what we're doing.
+  // with "modalOptions!: ModalOptions" (no | undefined) lint is warning us that this variable may show up as undefined later in our code. but because we as the dev know that it's defined within the ngOnInit, we add the ! to tell lint to go away; 'we know what we're doing'.
   constructor(private modalService: ModalService) {}
 
   ngOnInit() {
