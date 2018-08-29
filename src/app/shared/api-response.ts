b220b64a5b16b3aefbfa79306ba24d594cb03bd0
Bethany Warner <beth@wiredviews.com>
2018-08-29 16:43:39 -0400

    perf(naming convention): create unused interface for possibly refactoring down the road

---
+++ src/app/shared/api-response.ts
@@ -0,0 +1,5 @@
+export interface IApiResponse<T> {
+  results: T[];
+}
+
+// This is an option for naming convention purposes. When final refactoring occurs, I might pursue this. I leave this file as at least a record of possibility for upcoming projects.
